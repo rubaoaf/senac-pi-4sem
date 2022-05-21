bb80b127b64e8726125d98de7ae6a20e46bd6caa
Rubens Andreazzi Filho <rubens.filho@viavarejo.com.br>
2022-05-21 18:49:04 -0300

    inicio do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src='' className='App-logo' alt='logo' />
       </header>
+      <main>
+        <h1>O que você precisa?</h1>
+        <p>
+          Procure entre os melhores pet shops, clinicas veterinárias, casas de
+          ração, hotéis, escolinhas e demais serviços para seu pet.
+        </p>
+        <form>
+          <label htmlFor='lookingfor'>
+            <input
+              type='text'
+              name='lookingfor'
+              onChange={() => console.log('onChange')}
+              id='lookingfor'
+              placeholder='Estou procurando'
+            />
+          </label>
+          <label htmlFor='wherefind'>
+            <input
+              type='text'
+              name='wherefind'
+              onChange={() => console.log('onChange')}
+              id='wherefind'
+              placeholder='Cidade ou CEP'
+            />
+          </label>
+          <button type='submit'>Pesquisar</button>
+        </form>
+      </main>
     </div>
   );
 }
